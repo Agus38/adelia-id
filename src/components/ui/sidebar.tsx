fbc63efc5c7db3c656e8b3c880b1835845f65fe7
Admin <server64462@gmail.com>
2025-08-19 11:42:10 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -207,7 +207,9 @@ const Sidebar = React.forwardRef<
             }
             side={side}
           >
-            
+             <SheetHeader className="p-0 m-0 h-0">
+               <SheetTitle className="sr-only">Menu</SheetTitle>
+             </SheetHeader>
             <div className="flex h-full w-full flex-col">{children}</div>
              <SheetClose className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
                   <X className="h-4 w-4" />
