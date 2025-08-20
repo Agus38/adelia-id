524bdbd398fe3215d639b370006a49659902aab5
Admin <server64462@gmail.com>
2025-08-20 03:52:49 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Input value="" cl

--- src/app/stok-produk/page.tsx
+++ src/app/stok-produk/page.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 
 import * as React from 'react';
@@ -132,21 +131,21 @@ export default function StokProdukPage() {
                     <Input 
                         value={item.morning} 
                         onChange={(e) => handleStockChange(item.id, 'morning', e.target.value)}
-                        className="h-7"
+                        className="h-7 border-0 shadow-none focus-visible:ring-0"
                     />
                     </TableCell>
                     <TableCell className="px-2 py-1 border-r">
                     <Input 
                         value={item.afternoon} 
                         onChange={(e) => handleStockChange(item.id, 'afternoon', e.target.value)}
-                        className="h-7"
+                        className="h-7 border-0 shadow-none focus-visible:ring-0"
                     />
                     </TableCell>
                     <TableCell className="px-2 py-1">
                         <Input 
                         value={item.order} 
                         onChange={(e) => handleStockChange(item.id, 'order', e.target.value)}
-                        className="h-7"
+                        className="h-7 border-0 shadow-none focus-visible:ring-0"
                     />
                     </TableCell>
                 </TableRow>
