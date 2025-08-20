23b663d9c63d8f8203fd67520a09146f62177c86
Admin <server64462@gmail.com>
2025-08-20 05:23:56 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Input value="" cl

--- src/app/stok-produk/page.tsx
+++ src/app/stok-produk/page.tsx
@@ -126,21 +126,21 @@ export default function StokProdukPage() {
                     <Input 
                         value={item.morning} 
                         onChange={(e) => handleStockChange(item.id, 'morning', e.target.value)}
-                        className="h-8"
+                        className="h-8 bg-transparent"
                     />
                     </TableCell>
                     <TableCell className="px-1 border-r py-0">
                     <Input 
                         value={item.afternoon} 
                         onChange={(e) => handleStockChange(item.id, 'afternoon', e.target.value)}
-                        className="h-8"
+                        className="h-8 bg-transparent"
                     />
                     </TableCell>
                     <TableCell className="px-1 py-0">
                         <Input 
                         value={item.order} 
                         onChange={(e) => handleStockChange(item.id, 'order', e.target.value)}
-                        className="h-8"
+                        className="h-8 bg-transparent"
                     />
                     </TableCell>
                 </TableRow>
