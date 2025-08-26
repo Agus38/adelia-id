caedcd4693f18119a3c31a5124ba06bc4e8a8ffa
Admin <server64462@gmail.com>
2025-08-26 15:32:00 +0000

    Implementasi semua fitur yang ada di halaman (profile)

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import { Separator } from "@/components/ui/separator";
@@ -46,10 +47,10 @@ export default function SettingsProfilePage() {
     <div className="flex-1 space-y-4 p-4 pt-6 md:p-8">
        <div className="flex items-center space-x-2">
         <User className="h-8 w-8" />
-        <h2 className="text-3xl font-bold tracking-tight">Profil Pengguna</h2>
+        <h2 className="text-3xl font-bold tracking-tight">Pengaturan Akun</h2>
       </div>
        <p className="text-muted-foreground">
-        Kelola pengaturan akun dan preferensi Anda.
+        Kelola pengaturan akun, profil, dan preferensi Anda.
       </p>
       <Separator />
       <div className="space-y-6">
