f2503685b6dcb4fae18c52500bb4940dc74571b1
Admin <server64462@gmail.com>
2025-09-27 07:28:07 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -19,7 +19,7 @@ export function Toaster() {
     <ToastProvider>
       {toasts.map(function ({ id, title, description, action, ...props }) {
         return (
-          <Toast key={id} {...props}>
+          <Toast key={id} {...props} className={cn("mt-16 sm:mt-0", props.className)}>
             <div className="grid gap-1">
               {title && <ToastTitle>{title}</ToastTitle>}
               {description && (
