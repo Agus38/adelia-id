38aefa76605259b48513cd10a8397a293c02b7bc
Admin <server64462@gmail.com>
2025-09-27 19:36:13 +0000

    Update kembali, ketika saya uji coba di ponsel saya, popup notifikasi mu

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -17,8 +17,9 @@ const ToastViewport = React.forwardRef<
   <ToastPrimitives.Viewport
     ref={ref}
     className={cn(
-      "fixed z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:flex-col md:max-w-[420px]",
-      "top-0 left-1/2 -translate-x-1/2 sm:left-auto sm:right-0 sm:top-auto sm:bottom-0 sm:translate-x-0",
+      "fixed z-[100] flex max-h-screen w-full flex-col-reverse p-4",
+      "top-16 left-1/2 -translate-x-1/2",
+      "sm:left-auto sm:top-auto sm:right-0 sm:bottom-0 sm:translate-x-0 sm:flex-col md:max-w-[420px]",
       className
     )}
     {...props}
--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -29,7 +29,7 @@ export function Toaster() {
     <ToastProvider>
       {toasts.map(function ({ id, title, description, action, ...props }) {
         return (
-          <Toast key={id} {...props} className={cn("sm:mt-0 mt-16")}>
+          <Toast key={id} {...props}>
             <div className="grid gap-1">
               {title && <ToastTitle>{title}</ToastTitle>}
               {description && (
