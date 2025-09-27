e811f4e83e731d2cd5bebedd9bd25d6a23259db9
Admin <server64462@gmail.com>
2025-09-27 15:54:00 +0000

    Update kembali, kenapa popup notifikasi muncul di kanan atas, harusnya k

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -17,7 +17,8 @@ const ToastViewport = React.forwardRef<
   <ToastPrimitives.Viewport
     ref={ref}
     className={cn(
-      "fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:bottom-0 sm:right-0 sm:flex-col md:max-w-[420px]",
+      "fixed z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:flex-col md:max-w-[420px]",
+      "top-4 left-1/2 -translate-x-1/2 sm:left-auto sm:right-0 sm:top-auto sm:bottom-0 sm:translate-x-0",
       className
     )}
     {...props}
