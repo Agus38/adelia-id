3aac435d9963342e1806c554dbc4fd6f13adc403
Admin <server64462@gmail.com>
2025-10-22 05:49:20 +0000

    Apakah web app kita bisa menangani ini:
    Pastikan CSP efektif melawan se

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,34 @@
 
 import type { NextConfig } from 'next';
 
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval';
+    style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+    img-src 'self' data: https://placehold.co https://images.unsplash.com https://plus.unsplash.com https://lh3.googleusercontent.com https://openweathermap.org;
+    font-src 'self' https://fonts.gstatic.com;
+    connect-src 'self' https://api.myquran.com https://api.openweathermap.org;
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+`;
+
 const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\s{2,}/g, ' ').trim(),
+          },
+        ],
+      },
+    ]
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
