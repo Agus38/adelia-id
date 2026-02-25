8a017690c1cfd39a251fcad54b83fab6b014e970
A905 <43935936+Agus38@users.noreply.github.com>
2026-02-26 01:00:41 +0700

    Upload src/app/main-layout.tsx via Zip Uploader

---
+++ src/app/main-layout.tsx
@@ -0,0 +1,51 @@
+
+'use client';
+
+import { Header } from '@/components/layout/header';
+import { Toaster } from '@/components/ui/toaster';
+import { Footer } from '@/components/layout/footer';
+import { useEffect } from 'react';
+import type { User } from 'firebase/auth';
+import { useSessionManager } from '@/hooks/use-session-manager';
+import { AppSidebar } from '@/components/layout/sidebar';
+import { useUserStore } from '@/lib/user-store';
+import { useBudgetflowData } from '@/lib/budgetflow-store';
+import { usePresence } from '@/hooks/use-presence';
+import { FirebaseErrorListener } from '@/components/FirebaseErrorListener';
+
+export type UserProfile = User & {
+  id?: string;
+  fullName?: string;
+  role?: string;
+  avatarUrl?: string;
+  status?: 'Aktif' | 'Diblokir';
+};
+
+export function MainLayout({ children }: { children: React.ReactNode }) {
+  const { initializeUserListener } = useUserStore();
+  
+  // This hook will initialize the BudgetFlow data listeners when a user logs in.
+  useBudgetflowData();
+  useSessionManager();
+  usePresence(); // Initialize the presence system
+
+  useEffect(() => {
+    const unsubscribe = initializeUserListener();
+    return () => unsubscribe();
+  }, [initializeUserListener]);
+
+  return (
+    <>
+      <FirebaseErrorListener />
+      <div className="flex min-h-screen w-full flex-col bg-muted/40">
+          <Header />
+           <div className="flex flex-1">
+             <AppSidebar />
+             <main className="flex-1">{children}</main>
+          </div>
+          <Footer />
+      </div>
+      <Toaster />
+    </>
+  );
+}
