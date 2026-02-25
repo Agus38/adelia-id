253b4ade6ddd3318f6e95dfb88c32f8beaac326b
A905 <43935936+Agus38@users.noreply.github.com>
2026-02-26 00:59:15 +0700

    Upload src/app/admin/page.tsx via Zip Uploader

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,15 @@
+
+import { AdminDashboard } from "@/components/admin/admin-dashboard";
+
+export const dynamic = 'force-dynamic';
+
+export default function AdminDashboardPage() {
+  return (
+    <div className="flex-1 space-y-4 p-4 pt-6 md:p-8">
+      <div className="flex items-center justify-between space-y-2">
+        <h2 className="text-3xl font-bold tracking-tight">Dasbor Admin</h2>
+      </div>
+      <AdminDashboard />
+    </div>
+  );
+}
